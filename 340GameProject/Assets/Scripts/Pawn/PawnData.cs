1ca76e260fbb09ff7c8a0d27aea5221cf8f1793c
Elijah Nutter <enutter24@gmail.com>
2021-06-06 12:25:52 -0700

    MoreDetailedInformation

--- 340GameProject/Assets/Scripts/Pawn/PawnData.cs
+++ 340GameProject/Assets/Scripts/Pawn/PawnData.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PawnData : MonoBehaviour
 {
-    //variables needed for all pawns, characters and AI
-    protected Animator anim;
+    ///<Summary>
+    ///All the variables that are needed for every pawn, player or ai
+    ///</Summary>
+
+    #region Speed Variables
+    ///<Summary>
+    ///The different speeds needed to allow the player to move or turn
+    ///</Summary>
 
     [Header("Speeds")]
+
+    [Tooltip("Speeds the pawn up when they are on the ground")]
     [SerializeField] protected float speed = 4;
+
+    [Tooltip("The speed the pawn can rotate to a given point")] 
     [SerializeField] protected float turnSpeed = 180;
+    #endregion
+
+    #region Non-Accessible Variables
+    ///<Summary>
+    ///All of the variables that aren't accessible to other scripts
+    ///</Summary>
+
+    protected Animator anim; //gets the animatior on the pawn
+    #endregion
+
+    #region Awake Function
+    ///<Summary>
+    ///The awake function sets any of the variables that need to be set at the start of the game
+    ///</Summary>
 
     private void Awake()
     {
-        anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>(); //sets the animator
     }
+    #endregion
 }
--- 340GameProject/Assets/Scripts/Player/PlayerData.cs
+++ 340GameProject/Assets/Scripts/Player/PlayerData.cs
@@ -2,30 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerData : Pawn
 {
-    //data needed for player characters, but not ai
+    ///<Summary>
+    ///All of the variables that are player specific, variables that other pawns won't need
+    ///</Summary>
+
+    #region Camera Variables
+    ///<Summary>
+    ///The camera that will allow the camera to follow the player so they can see what they are doing
+    ///</Summary>
 
     [Header("Camera")]
+
+    [Tooltip("The main camera in the scene, allows the camera to follow the player")] 
     [SerializeField] protected Camera playerCam;
+    #endregion
+
+    #region Jumping / Falling Variables
+    ///<Summary>
+    ///All the variables needed to control the falling and jumping movement
+    ///</Summary>
 
     [Header("Jumping / Falling")]
+
+    [Tooltip("The force applied upwards when the player jumps")]
     [SerializeField] protected float jumpheight = 3f;
+    
+    [Tooltip("The force that pulls the player down when they are in the air")] 
     [SerializeField] protected float gravity = 20f;
-    [SerializeField] protected float stepDown = 0.1f;
+    
+    [Tooltip("The amount the player can move in the air")] 
     [SerializeField] protected float airControl = 2.5f;
+    
+    [Tooltip("The amount the jump is smoothed")] 
     [SerializeField] protected float jumpDamp = 2.5f;
+    #endregion
 
+    #region Movement / Pushing Variables
+    ///<Summary>
+    ///All the variables needed control the movement and the pushing that are player specific
+    ///</Summary>
+
+    [Header("Movement / Pushing")]
+
+    [Tooltip("The amount the player can step down (stairs)")] 
+    [SerializeField] protected float stepDown = 0.1f;
+    
+    [Tooltip("Speeds the player up on the ground")] 
     [SerializeField] protected float groundSpeed = 1f;
+    
+    [Tooltip("Amount of force applied to objects with rigidbodies")] 
     [SerializeField] protected float pushPower = 2.0F;
+    #endregion
 
-    protected Vector3 rootMotion;
-    protected Vector3 velocity;
-    protected bool isJumping;
-    protected CharacterController charCont;
+    #region Non-Accessible Variables
+    ///<Summary>
+    ///All of the variables that are not accessible from other scripts
+    ///</Summary>
+
+    protected Vector3 rootMotion; //Adds all the root motion from the anim to use later
+    protected Vector3 velocity; //Used to get and set the velocity of the player
+    protected bool isJumping; //Checks if the player is jumping or not
+    protected CharacterController charCont; //Gets the character controller to access it
+    #endregion
+
+    #region Start Function
+    ///<Summary>
+    ///Start function sets any of the varaibles that need to be set at the start of the game
+    ///</Summary>
 
     private void Start()
     {
-        charCont = GetComponent<CharacterController>();
+        charCont = GetComponent<CharacterController>(); //Sets the character controller
     }
+    #endregion
 }
